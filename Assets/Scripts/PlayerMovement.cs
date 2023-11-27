e21c9f8455aad2d3cc4cf9db455f809a71c469f5
Chris-Chen <61469920+chrischen842@users.noreply.github.com>
2023-11-27 16:36:30 -0500

    Player movement and Animation
    Added player jump, double jump, run, idle, and fall movement and animations

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,23 +3,94 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
-    public float jumpSpeed = 5f;
-    private Rigidbody2D rb;
+    public float jumpSpeed = 9f;
+
+    private Rigidbody2D _Rigidbody2D;
+    private SpriteRenderer _SpriteRenderer;
+    private Animator _Animator;
     private Vector2 movement;
+
+    private enum MovementState { idle, running, jumping, falling, doubleJump };
+    private MovementState state;
+    private int jumpCount = 0;  // Counter for the number of jumps
 
     private void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        _Rigidbody2D = GetComponent<Rigidbody2D>();
+        _SpriteRenderer = GetComponent<SpriteRenderer>();
+        _Animator = GetComponent<Animator>();
     }
 
-    void Update()
+    private void Update()
     {
-        // Input
         movement.x = Input.GetAxisRaw("Horizontal");
-        rb.velocity = new Vector2(movement.x * moveSpeed, rb.velocity.y);
+        _Rigidbody2D.velocity = new Vector2(movement.x * moveSpeed, _Rigidbody2D.velocity.y);
+
+        // Jump logic
         if (Input.GetButtonDown("Jump"))
         {
-            rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
+            if (jumpCount < 2) // Allow double jump
+            {
+                _Rigidbody2D.velocity = new Vector2(_Rigidbody2D.velocity.x, jumpSpeed);
+                jumpCount++;
+
+                if (jumpCount == 2)
+                {
+                    state = MovementState.doubleJump;
+                    _Animator.SetInteger("state", (int)state);
+                }
+            }
+        }
+
+        UpdateAnimation();
+    }
+
+    private void UpdateAnimation()
+    {
+        if (movement.x > 0f)
+        {
+            if(state != MovementState.doubleJump)
+            {
+                state = MovementState.running;
+            }
+            
+            _SpriteRenderer.flipX = false;
+        }
+        else if (movement.x < 0f)
+        {
+            if (state != MovementState.doubleJump)
+            {
+                state = MovementState.running;
+            }
+
+            _SpriteRenderer.flipX = true;
+        }
+        else if (jumpCount == 0)
+        {
+            state = MovementState.idle;
+        }
+
+        if (_Rigidbody2D.velocity.y > 0.1f && jumpCount == 1)
+        {
+            state = MovementState.jumping;
+        }
+        else if (_Rigidbody2D.velocity.y < -0.1f)
+        {
+            state = MovementState.falling;
+        }
+
+        if (state != MovementState.doubleJump) // Avoid overriding doubleJump state
+        {
+            _Animator.SetInteger("state", (int)state);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Reset jump count when landing
+        if (collision.gameObject.tag == "Ground")
+        {
+            jumpCount = 0;
         }
     }
 }
